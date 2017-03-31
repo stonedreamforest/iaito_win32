15d67a7acbbcef3e01acdb0a0986fe4d2468bb1f
mrexodia <mr.exodia.tpodt@gmail.com>
2017-03-31 10:41:07 +0200

    updated to r2 4715f1e

--- radare2/include/libr/r_version.h
+++ radare2/include/libr/r_version.h
@@ -1,7 +1,7 @@
 #ifndef R_VERSION_H
 #define R_VERSION_H 1
-#define R2_VERSION_COMMIT 14725
-#define R2_GITTAP "1.3.0-213-gcd2874404"
-#define R2_GITTIP "cd28744049492dc8ac25a1f2b3ba0e42f0e9ce93"
-#define R2_BIRTH "2017-03-30__02:42:12"
+#define R2_VERSION_COMMIT 14740
+#define R2_GITTAP "1.3.0-228-g4715f1e2b"
+#define R2_GITTIP "4715f1e2b9394aac832ff5f37e6a7504c59fd993"
+#define R2_BIRTH "2017-03-31__10:34:35"
 #endif
